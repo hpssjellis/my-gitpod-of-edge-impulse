54d880d677ca47b544a029c0c0736713cc86eabf
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-09 23:17:08 -0800

    Update a01-c.sh

--- edge-impulse/local-build-firmware/c/a01-c.sh
+++ edge-impulse/local-build-firmware/c/a01-c.sh
@@ -1,1 +1,24 @@
-zdfs
+#!/bin/bash
+
+echo "Upload your edge impulse deployed without EON compiler C++ zipped library to this directory and rename it as edge.zip"
+echo "Good idea to duplicate this folder so you can easily upload a different program."
+echo "enter to contrinue, ctrl-C to quit"
+read
+
+
+
+echo "unzipping and putting all the files into this file structure"
+unzip edge.zip 
+echo "In the src folder check the main.cpp file and make changes or replace it with a different one"
+echo "If using the default Edge Impulse program you will need to insert features in the array. see try me folder"
+echo "enter to contrinue, ctrl-C to quit"
+read
+
+
+
+echo "Compiling your complete impulse"
+echo "This is the most likely place to have issues"
+make -j
+
+
+echo "Now get your program onto your board. "
