45b161c440dbb8f10a43a2ffd44e08d6d381fee7
Jeremy Ellis <keyfreemusic@gmail.com>
2021-06-06 06:52:07 -0700

    Update a01-nano.sh

--- edge-impulse/local-build-firmware/nano33bleSense/a01-nano.sh
+++ edge-impulse/local-build-firmware/nano33bleSense/a01-nano.sh
@@ -1,25 +1,9 @@
 
 #!/bin/bash
 
-echo "Upload your edge impulse deployed without EON compiler C++ zipped library to this directory and rename it as edge.zip"
-echo "Good idea to duplicate this folder so you can easily upload a different program."
-echo "enter to contrinue, ctrl-C to quit"
-read
+echo "Just make sure the aurdino-build.sh file is in the same folder as your unzipped arduino edge impulse folder"
 
-
-
-echo "unzipping and putting all the files into this file structure"
-unzip edge.zip 
-echo "In the src folder check the main.cpp file and make changes or replace it with a different one"
-echo "If using the default Edge Impulse program you will need to insert features in the array. see try me folder"
-echo "enter to contrinue, ctrl-C to quit"
-read
-
-
-
-echo "Compiling your complete impulse"
-echo "This is the most likely place to have issues"
-make -j
+./arduino-build.sh
 
 
 echo "Copying the build/edge_impulse_inferencing_bootloader.bin to the downloadable folder eta-edge-serial-upload "
