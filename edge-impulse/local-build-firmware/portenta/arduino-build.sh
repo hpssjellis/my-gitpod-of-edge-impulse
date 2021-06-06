23921517e878ce8a99bd11fe1ff358b80c093ce6
Jeremy Ellis <keyfreemusic@gmail.com>
2021-06-05 23:02:20 -0700

    Update arduino-build.sh

--- edge-impulse/local-build-firmware/portenta/arduino-build.sh
+++ edge-impulse/local-build-firmware/portenta/arduino-build.sh
@@ -38,7 +38,7 @@ HAS_ARDUINO_CORE="$(has_arduino_core)"
 if [ -z "$HAS_ARDUINO_CORE" ]; then
     echo "Installing Arduino Mbed core..."
     $ARDUINO_CLI core update-index
-    $ARDUINO_CLI core install arduino:mbed@1.3.2
+    $ARDUINO_CLI core install arduino:mbed@2.1.0
     echo "Installing Arduino Mbed core OK"
 fi
 
